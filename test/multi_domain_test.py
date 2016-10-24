1e10a90eeace05fffad45feeb38d41454348840d
Yoshinari Fujinuma <fujinumay@gmail.com>
2016-10-23 19:39:38 -0600

    Adding documentations to multi_domain_test

--- test/multi_domain_test.py
+++ test/multi_domain_test.py
@@ -3,6 +3,7 @@ from sklearn.multiclass import OneVsRestClassifier
 from sklearn import linear_model
 from evaluation_metrics.coherence.coherence import Coherence
 import numpy as np
+from six.moves import xrange
 
 
 def make_classifiers(clfs, penalties, Cs):
@@ -22,23 +23,34 @@ def make_classifiers(clfs, penalties, Cs):
 
 
 # domains = ['books','dvd','electronics','kitchen']
-domains = ['kitchen']
+domains = ['electronics', 'kitchen']
 data = []
 for domain in domains:
     data.append(multi_domain_sentiment.load(domain))
 
 N = len(domains)
 outData = []
-penalties = ['l1', 'l2']
-Cs = [0.01, 0.1, 1.0, 10.0, 100.0, 1000.0]
+# penalties = ['l1', 'l2']
+penalties = ['l1']
+# Cs = [0.01, 0.1, 1.0, 10.0, 100.0, 1000.0]
+Cs = [1.0]
 clfs = make_classifiers(["LogisticRegression"], penalties, Cs)
-print(len(clfs))
+print("Num. of different classifiers that we will test: %s" % len(clfs))
 cohere = Coherence(type='OC-Auto-NPMI')
-for clf in clfs:
+"""
+OC-Auto-NPMI is pairwise PMI of top-N topic words.
+See http://www.aclweb.org/anthology/E14-1056 for more details.
+"""
+for i, clf in enumerate(clfs):
+    print("Classifier %s" % i)
     for i in xrange(N):
         clf.fit(data[i].X, data[i].y)
+        print(data[i].X, data[i].y)
+        """
+        Calculate the coherence score for each class
+        """
         cScore = cohere.score(clf, data[i].X, data[i].y, fit=False)
-    print("classifier score: %s" % cScore)
+    print("coherence score: %s" % cScore)
     for j in xrange(N):
         if j != i:
             aScore = clf.score(data[j].X, data[j].y)
